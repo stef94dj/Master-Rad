018831ce19ba9f62ae04b087b28fcce311f6cc12
Stefan <stefan.djordjevic@tnation.eu>
2019-07-20 18:29:42 +0200

    added page to setup a new DB

---
+++ MasterRad/Controllers/SetupController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MasterRad.Models;
+
+namespace MasterRad.Controllers
+{
+    public class SetupController : Controller
+    {
+        public IActionResult MyDatabases()
+        {
+            return View();
+        }
+
+        public IActionResult MyDatabase()
+        {
+            return View();
+        }
+    }
+}
