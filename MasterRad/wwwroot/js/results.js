d5edec28401d0c76826c0c3b40617cf2639e9e8b
Stefan Djordjevic <Stefan.Djordjevic@flagstoneim.com>
2019-12-21 23:19:55 +0100

    ui & icons (wip)

---
+++ MasterRad/wwwroot/js/results.js
@@ -0,0 +1,30 @@
+﻿var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/jobprogress")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("progress",
+    (percent) => {
+        if (percent === 100) {
+            document.getElementById("job-status").innerText = "Finished!";
+        } else {
+            document.getElementById("job-status").innerText = `${percent}%`;
+        }
+    });
+
+connection.start()
+    .then(_ => connection.invoke("AssociateJob", "123"))
+    .catch(err => console.error(err.toString()));
+
+function startProgress() {
+    $.ajax({
+        url: '/api/Evaluate/StartProgress',
+        dataType: 'json',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function (data, textStatus, jQxhr) {
+            alert('started');
+        }
+    });
+}
