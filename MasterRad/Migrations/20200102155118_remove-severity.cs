508909738856462cb27f5f7b42b94e21d6015bda
Stefan Djordjevic <Stefan.Djordjevic@flagstoneim.com>
2020-01-02 16:56:42 +0100

    added missing migration

---
+++ MasterRad/Migrations/20200102155118_remove-severity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MasterRad.Migrations
+{
+    public partial class removeseverity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Severity",
+                table: "ExceptionLog");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Severity",
+                table: "ExceptionLog",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
