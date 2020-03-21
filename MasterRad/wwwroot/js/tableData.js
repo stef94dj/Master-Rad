f2aaa7ee3c8e2dd1a112d5c2ad99e57a472c696d
Stefan Djordjevic <Stefan.Djordjevic@flagstoneim.com>
2020-03-21 16:36:28 +0100

    table info: fix on-load not showing + format json + format json-viewer library code

--- MasterRad/wwwroot/js/tableData.js
+++ MasterRad/wwwroot/js/tableData.js
@@ -1,20 +1,59 @@
 ﻿function loadJson(dbName, schemaName, tableName) {
-    var apiUrl = '/api/Metadata/explore/' + dbName + '/' + schemaName + '/' + tableName;
-
-    $.ajax({
-        url: apiUrl,
-        type: 'GET',
-        success: function (data) {
+    getJson(dbName, schemaName, tableName)
+        .then(data => {
             displayJson(data);
-        }
-    });
+        })
 }
+
+function getJson(dbName, schemaName, tableName) {
+    var apiUrl = '/api/Metadata/explore/' + dbName + '/' + schemaName + '/' + tableName;
+    return promisifyAjaxGet(apiUrl)
+}
+
 function displayJson(jsonData) {
     var input = eval('(' + JSON.stringify(jsonData) + ')');
+
+    input = mapToDisplay(input);
+
     $('#json-display').jsonViewer(input, {
         collapsed: true,
         rootCollapsable: false
     });
+}
+
+function mapToDisplay(inputJson) {
+    var res = {
+        columns: {},
+        constraints: {}
+    };
+
+    if (inputJson == null)
+        return res;
+
+    if (inputJson.columns != null) {
+        $.each(inputJson.columns, function (index, item) {
+            var colObj = {
+                type: item.type,
+                isNullable: item.isNullable,
+                defaultValue: item.defaultValue,
+                maxLength: item.maxLength
+            };
+            res.columns[item.name] = colObj;
+        });
+    }
+
+    if (inputJson.constraints != null) {
+        $.each(inputJson.constraints, function (index, item) {
+            var consObj = {
+                type: item.type,
+                description: item.description,
+            };
+            res.constraints[item.name] = consObj;
+        });
+    }
+
+    debugger;
+    return res;
 }
 
 function hideTableData() {
