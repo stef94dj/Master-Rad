8380ebfc70d5db6ddde7b4362dce4dbcc3a82196
Stefan Djordjevic <Stefan.Djordjevic@flagstoneim.com>
2020-03-28 21:36:46 +0100

    LogInPartial styling, page width, menu items when authenthicated, menu items based on role

--- MasterRad/Constants.cs
+++ MasterRad/Constants.cs
@@ -7,9 +7,11 @@ namespace MasterRad
 {
     public static class Constants
     {
+        public const string BearerAuthorizationScheme = "Bearer";
         public const string ScopeUserRead = "User.Read";
         public const string ScopeUserReadBasicAll = "User.ReadBasic.All";
-        public const string BearerAuthorizationScheme = "Bearer";
         public const string MicrosoftSQLConnectionStringTemplate = "Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
+        public const string RoleProfessor = "Professor";
+        public const string RoleStudent = "Student";
     }
 }
