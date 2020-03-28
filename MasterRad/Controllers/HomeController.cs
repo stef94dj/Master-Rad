266afe5c246a760dba9b3e5dff9400336c544c3a
Stefan Djordjevic <Stefan.Djordjevic@flagstoneim.com>
2020-03-28 16:42:48 +0100

    overwrite with new 31 core template + appsettings for call ms graph

--- MasterRad/Controllers/HomeController.cs
+++ MasterRad/Controllers/HomeController.cs
@@ -4,21 +4,34 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MasterRad.Models;
 
 namespace MasterRad.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
-            => View();
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
-            => View();
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- MasterRad/Startup.cs
+++ MasterRad/Startup.cs
@@ -1,22 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Coravel;
-using MasterRad.Extensions;
-using MasterRad.Models;
-using MasterRad.Repositories;
-using MasterRad.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MasterRad
 {
@@ -32,43 +23,11 @@ namespace MasterRad
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            var conf = new ConnectionParams();
-            Configuration.Bind("DbAdminConnection", conf);
-            var connString = $"server={conf.ServerName};database={conf.DbName};User ID={conf.Login};password={conf.Password};";
-            services.AddDbContext<Context>(opts => opts.UseSqlServer(new SqlConnection(connString)));
-
-            //services
-            services.AddScoped<IMicrosoftSQL, MicrosoftSQL>();
-            services.AddSingleton<IMsSqlQueryBuilder, MsSqlQueryBuilder>();
-            services.AddScoped<IUser, User>();
-            services.AddScoped<IEvaluator, Evaluator>();
-            services.AddScoped(typeof(ISignalR<>), typeof(SignalR<>));
-            services.AddScoped<ILogRepository, LogRepository>();
-
-            //repositories
-            services.AddScoped<ITemplateRepository, TemplateRepository>();
-            services.AddScoped<ITaskRepository, TaskRepository>();
-            services.AddScoped<ISynthesisRepository, SynthesisRepository>();
-            services.AddScoped<IAnalysisRepository, AnalysisRepository>();
-            services.AddScoped<IStudentRepository, StudentRepository>();
-
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
-            services.AddQueue();
-            services.AddSignalR();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -80,29 +39,19 @@ namespace MasterRad
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.AddCustomExceptionMiddleware();
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<SynthesisProgressHub>("/synthesisprogress");
-                routes.MapHub<AnalysisProgressHub>("/analysisprogress");
-            });
+            app.UseRouting();
 
-            app.UseMvc(routes =>
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-
-            var appDataPath = $"{AppContext.BaseDirectory}\\AppData";
-            if (!Directory.Exists(appDataPath))
-                Directory.CreateDirectory(appDataPath);
         }
     }
 }
