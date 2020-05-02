0c66f4a5156c08e94b3563de9646abc0be10227b
Stefan Djordjevic <stefan.djordjevic@flagstoneim.com>
2020-05-02 15:52:03 +0200

    wip

--- MasterRad/wwwroot/js/pagination.js
+++ MasterRad/wwwroot/js/pagination.js
@@ -4,8 +4,14 @@
     initUI: function (tableThSelector, filterThSelector) {
         tableTh = $(tableThSelector);
         filterTh = $(filterThSelector);
+        this.initPageSize();
         this.initSort();
         this.initTextFilters();
+    },
+    initPageSize: function () {
+        $('#page-size').change(function () {
+            pagination.triggerSearch('page size change');
+        })
     },
     initSort: function () {
         sortIcons = this.getSortIcons();
@@ -16,7 +22,18 @@
         });
     },
     initTextFilters: function () {
-
+        var inputs = this.getFilterTextInputs();
+        $.each(inputs, function (index, item) {
+            $(item).keyup(throttle(function () {
+                pagination.textInputHandler(this);
+            }, 750));
+        })
+    },
+    getSortIcons: function () {
+        return tableTh.find('i.sort-icon');
+    },
+    getFilterTextInputs: function () {
+        return filterTh.find('input.filter-text-input');
     },
     sortClickHandler: function (clickedThCell) {
         var clickedSortIcon = $($(clickedThCell).find('i.sort-icon')[0]);
@@ -38,8 +55,26 @@
             clickedSortIcon.removeClass('fa-sort');
             clickedSortIcon.addClass(`fa-sort-${nextState}`);
         }
+
+        pagination.triggerSearch(`sort`);
     },
-    getSortIcons: function () {
-        return tableTh.find('i.sort-icon');
+    textInputHandler: function (input) {
+        var placeholderTxt = $(input).attr('placeholder');
+        pagination.triggerSearch(`filter ${placeholderTxt}`);
+    },
+    triggerSearch: function (triggeredBy) {
+        alert(`search triggerred by ${triggeredBy}`);
     }
+}
+
+function throttle(f, delay) {
+    var timer = null;
+    return function () {
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = window.setTimeout(function () {
+            f.apply(context, args);
+        },
+            delay || 500);
+    };
 }
