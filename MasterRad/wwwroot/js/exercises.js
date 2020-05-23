05a64cadb0a0e8361995fb2c14070ca1ab2e482c
Stefan Djordjevic <stefan.djordjevic@flagstoneim.com>
2020-05-23 19:40:00 +0200

    Exercises page UI

--- MasterRad/wwwroot/js/exercises.js
+++ MasterRad/wwwroot/js/exercises.js
@@ -1,3 +1,18 @@
-﻿$(document).ready(function () {
+﻿var nameModal = null;
+$(document).ready(function () {
     setActive("Exercises");
+
+    initializeTooltips();
+
+    nameModal = nameModalBuilder.BuildHandler();
+    nameModal.Init('#create-instance-modal', onInstanceModalShow, createInstance);
 });
+
+
+function onInstanceModalShow() {
+
+}
+
+function createInstance() {
+
+}
